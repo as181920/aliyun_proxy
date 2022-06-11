9aa926bc0d4a26ffa10e4e6db856283709298eb2
Andersen Fan <as181920@gmail.com>
2022-06-11 15:31:30 +0800

    Add sms callback handle fillers

---
+++ app/controllers/aliyun_proxy/sms/callback/message_reports_controller.rb
@@ -0,0 +1,7 @@
+module AliyunProxy
+  class Sms::Callback::MessageReportsController < ApplicationController
+    def create
+      render json: { code: 0, msg: "成功" }, status: :ok
+    end
+  end
+end
