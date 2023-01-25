0b65ca5a1fe2262108e40bf62c4829519291f637
Andersen Fan <as181920@gmail.com>
2023-01-25 21:19:22 +0800

    Handle sms sign state update on callback report

--- app/controllers/aliyun_proxy/sms/callback/sign_reports_controller.rb
+++ app/controllers/aliyun_proxy/sms/callback/sign_reports_controller.rb
@@ -1,7 +1,21 @@
 module AliyunProxy
   class Sms::Callback::SignReportsController < ApplicationController
+    after_action :handle_sign_state_change, only: [:create]
+
     def create
       render json: { code: 0, msg: "成功" }, status: :ok
     end
+
+    private
+
+      def handle_sign_state_change
+        params["_json"].each do |report_info|
+          Sms::Sign.find_by(name: report_info["sign_name"])&.then do |sign|
+            sign.update \
+              state: report_info["sign_status"],
+              reason: report_info["reason"]
+          end
+        end
+      end
   end
 end
