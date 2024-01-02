d8f17213f8393255c117cba94aa796cca805bc21
Andersen Fan <as181920@gmail.com>
2024-01-03 00:45:39 +0800

    Chore: add Pnvs::VerifySchemeService
    TODO: create with H5 should be verified

---
+++ app/services/aliyun_proxy/pnvs/verify_scheme_service.rb
@@ -0,0 +1,20 @@
+module AliyunProxy
+  module Pnvs
+    class VerifySchemeService < ApiBaseService
+      def create(params = { scheme_name: "一键登录认证方案", app_name: "阿里云通信", os_type: "H5", origin: "", url: "" })
+        client.request \
+          action: "CreateVerifyScheme",
+          params: params.stringify_keys.transform_keys(&:camelize),
+          opts: {}
+      end
+
+      # return:  => { "Message"=>"OK", "RequestId"=>"2CF8BD27-4A06-5C2F-8134-XXXXXXXXXXXX", "Code"=>"OK", "Result"=>true }
+      def destroy(scheme_code: "")
+        client.request \
+          action: "DeleteVerifyScheme",
+          params: { SchemeCode: scheme_code },
+          opts: {}
+      end
+    end
+  end
+end
