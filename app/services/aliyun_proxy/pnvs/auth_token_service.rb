9f83fc9a8f08198920c07c96338427bdeea4ca58
Andersen Fan <as181920@gmail.com>
2024-03-31 17:46:58 +0800

    Fix: add SceneCode on Pnvs::AuthTokenService

--- app/services/aliyun_proxy/pnvs/auth_token_service.rb
+++ app/services/aliyun_proxy/pnvs/auth_token_service.rb
@@ -1,10 +1,10 @@
 module AliyunProxy
   module Pnvs
     class AuthTokenService < ApiBaseService
-      def generate(url: "", origin: "")
+      def generate(url: "", origin: "", scene_code: nil)
         client.request \
           action: "GetAuthToken",
-          params: { Url: url, Origin: origin },
+          params: { Url: url, Origin: origin, SceneCode: scene_code }.compact_blank,
           opts: {}
       end
     end
