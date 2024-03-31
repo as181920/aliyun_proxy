f437ce593b4e2a973b8c916e49b79e6d1dff48bc
Andersen Fan <as181920@gmail.com>
2024-03-31 18:03:40 +0800

    Refactor: auto detect SceneCode on Pnvs/AuthTokenService

--- app/services/aliyun_proxy/pnvs/auth_token_service.rb
+++ app/services/aliyun_proxy/pnvs/auth_token_service.rb
@@ -4,9 +4,23 @@ module AliyunProxy
       def generate(url: "", origin: "", scene_code: nil)
         client.request \
           action: "GetAuthToken",
-          params: { Url: url, Origin: origin, SceneCode: scene_code }.compact_blank,
+          params: { Url: url, Origin: origin, SceneCode: scene_code.presence || detect_scene_code(url:, origin:) }.compact_blank,
           opts: {}
       end
+
+      private
+
+        def detect_scene_code(url: "", origin: "")
+          VerifyScheme.success.find_by \
+            scheme_name: URI.parse(url).host,
+            app_name: I18n.t("app_name", default: "CODE-LI"),
+            os_type: "Web",
+            origin:,
+            url:
+        rescue StandardError => e
+          Rails.logger.error "#{self.class.name} detect_scene_code #{e.class.name}: #{e.message}"
+          nil
+        end
     end
   end
 end
